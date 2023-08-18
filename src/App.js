4d1df0be4a5f32621ec063fd421062ec117122c2
ffurnes <ffurnes>
2023-08-18 14:11:32 +0200

    changed text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Frank
+          Hello Frank, you are now on feature branch
         </p>
         <a
           className="App-link"
